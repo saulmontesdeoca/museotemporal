918c03b86a08d60969d8151dc53c0b86e034e891
Saul Montes De Oca <mr.sauce97@gmail.com>
2020-10-29 18:49:52 -0600

    🦾 Project structure

---
+++ js/main.js
@@ -0,0 +1,168 @@
+let camera, scene, renderer, controls;
+
+let objects = [];
+let blocker,  instructions;
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+
+let prevTime = performance.now();
+let velocity, direction;
+
+let floorUrl = "./js/assets/floor.jpg";
+
+function initPointerLock()
+{
+    blocker = document.getElementById( 'blocker' );
+    instructions = document.getElementById( 'instructions' );
+
+    controls = new THREE.PointerLockControls( camera, document.body );
+
+    controls.addEventListener( 'lock', function () {
+        instructions.style.display = 'none';
+        blocker.style.display = 'none';
+    } );
+    
+    controls.addEventListener( 'unlock', function () {
+        blocker.style.display = 'block';
+        instructions.style.display = '';
+    } );
+
+    instructions.addEventListener( 'click', function () {
+        controls.lock();
+    }, false );
+
+    scene.add( controls.getObject() );
+}
+
+function onKeyDown ( event )
+{
+    switch ( event.keyCode ) {
+
+        case 38: // up
+        case 87: // w
+            moveForward = true;
+            break;
+
+        case 37: // left
+        case 65: // a
+            moveLeft = true; 
+            break;
+
+        case 40: // down
+        case 83: // s
+            moveBackward = true;
+            break;
+
+        case 39: // right
+        case 68: // d
+            moveRight = true;
+            break;
+    }
+
+}
+
+function onKeyUp( event ) {
+
+    switch( event.keyCode ) {
+
+        case 38: // up
+        case 87: // w
+            moveForward = false;
+            break;
+
+        case 37: // left
+        case 65: // a
+            moveLeft = false;
+            break;
+
+        case 40: // down
+        case 83: // s
+            moveBackward = false;
+            break;
+
+        case 39: // right
+        case 68: // d
+            moveRight = false;
+            break;
+
+    }
+}
+
+function createScene(canvas) 
+{    
+    renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
+
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+    velocity = new THREE.Vector3();
+    direction = new THREE.Vector3();
+    
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.y = 64;
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xffffff );
+    scene.fog = new THREE.Fog( 0xffffff, 0, 550 );
+
+    let light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
+    light.position.set( 0.5, 1, 0.75 );
+    scene.add( light );
+
+    document.addEventListener( 'keydown', onKeyDown, false );
+    document.addEventListener( 'keyup', onKeyUp, false );
+
+    // floor
+    let map = new THREE.TextureLoader().load(floorUrl);
+    map.wrapS = map.wrapT = THREE.RepeatWrapping;
+    map.repeat.set(32, 32);
+
+    let floorGeometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
+    let floor = new THREE.Mesh(floorGeometry, new THREE.MeshPhongMaterial({color:0xffffff, map:map, side:THREE.DoubleSide}));
+    floor.rotation.x = -Math.PI / 2;
+    scene.add( floor );
+
+    initPointerLock();
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function run() 
+{
+    requestAnimationFrame( run );
+
+    if ( controls.isLocked === true ) 
+    {
+        let time = performance.now();
+        let delta = ( time - prevTime ) / 1000;
+
+        velocity.x -= velocity.x * 10.0 * delta;
+        velocity.z -= velocity.z * 10.0 * delta;
+
+        direction.z = Number( moveForward ) - Number( moveBackward );
+        direction.x = Number( moveRight ) - Number( moveLeft );
+
+        direction.normalize(); // this ensures consistent movements in all directions
+        if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
+        if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+
+        controls.moveRight( - velocity.x * delta );
+        controls.moveForward( - velocity.z * delta );
+        prevTime = time;
+    }
+
+    renderer.render( scene, camera );
+
+}
